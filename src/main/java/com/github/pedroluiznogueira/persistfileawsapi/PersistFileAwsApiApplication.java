da2546b167135cd68ea60a9678bd78a2b2ddfaca
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-01-13 18:20:40 -0300

    Initial commit

---
+++ src/main/java/com/github/pedroluiznogueira/persistfileawsapi/PersistFileAwsApiApplication.java
@@ -0,0 +1,14 @@
+package com.github.pedroluiznogueira.persistfileawsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersistFileAwsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersistFileAwsApiApplication.class, args);
+		System.out.println("Running...");
+	}
+
+}
---
+++ src/test/java/com/github/pedroluiznogueira/persistfileawsapi/PersistFileAwsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.pedroluiznogueira.persistfileawsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersistFileAwsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
